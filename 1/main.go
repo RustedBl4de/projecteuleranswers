940ee81eb876397c87227094d043d92c8eb0c391
RustedBl4de <rusty.chapin+github@gmail.com>
2022-10-24 08:40:34 -0700

    Added comments

--- 1/main.go
+++ 1/main.go
@@ -1,5 +1,3 @@
-package main
-
 /*
 Answers to projecteuler.net
 
@@ -7,7 +5,13 @@ Answers to projecteuler.net
 
 Multiples of 3 or 5
 Problem 1
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
 */
+
+package main
 
 import (
 	"fmt"
